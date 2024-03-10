39e35dfeb3a127d2aaa5ac428b3593f7492f7293
joseLuisGalvis <jolugales@gmail.com>
2024-03-10 05:27:57 -0300

    Ajuste Responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SectionOneComponent } from './components/section-one/section-one.component';
+import { SectionTwoComponent } from './components/section-two/section-two.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [
+    RouterOutlet,
+    NavBarComponent,
+    BodyComponent,
+    FooterComponent,
+    SectionOneComponent,
+    SectionTwoComponent
+  ],
+  standalone: true
 })
-export class AppComponent {
-  title = 'angFirstProj';
-}
+export class AppComponent {}
